67a37ec50c519267dc0032edd47f50d905644dd4
Dajana Mukaj <dajana@Dajanas-MacBook-Pro.local>
2024-09-28 01:36:52 -0400

    add border to tableDemo

--- src/components/TableDemo.jsx
+++ src/components/TableDemo.jsx
@@ -5,23 +5,23 @@ export default function TableDemo({ date }) {
     <main className="flex flex-col items-center ">
       <div className="flex flex-col lg:gap-6 gap-14 w-full xs:w-[100px]">
         <div className=" flex flex-col w-full md:space-y-0 md:gap-8 space-y-8 xl:grid xl:grid-cols-2 items-center justify-center">
-          <div className="flex flex-col items-center justify-center p-10 rounded-lg shadow-lg bg-card/60">
+          <div className="flex flex-col items-center justify-center p-10 rounded-lg border dark:border-white shadow-lg bg-card/60">
             <h1 className="text-lg font-semibold md:text-2xl my-4">
               Breakfast
             </h1>
             <FoodTable mealType="Breakfast" date={date} />
           </div>
-          <div className="flex flex-col items-center justify-center p-10 rounded-lg shadow-lg bg-card/50">
+          <div className="flex flex-col items-center justify-center p-10 rounded-lg border dark:border-white  shadow-lg bg-card/50">
             <h1 className="text-lg font-semibold md:text-2xl my-4">Lunch</h1>
 
             <FoodTable mealType="Lunch" date={date} />
           </div>
-          <div className="flex flex-col items-center justify-center rounded-lg p-10 shadow-lg bg-card/50">
+          <div className="flex flex-col items-center justify-center rounded-lg p-10 border dark:border-white  shadow-lg bg-card/50">
             <h1 className="text-lg font-semibold md:text-2xl my-4">Dinner</h1>
 
             <FoodTable mealType="Dinner" date={date} />
           </div>
-          <div className="flex flex-col items-center justify-center rounded-lg p-10 shadow-lg bg-card/50">
+          <div className="flex flex-col items-center justify-center rounded-lg p-10 border dark:border-white  shadow-lg bg-card/50">
             <h1 className="text-lg font-semibold md:text-2xl my-4">Snacks</h1>
 
             <FoodTable mealType="Snack" date={date} />
